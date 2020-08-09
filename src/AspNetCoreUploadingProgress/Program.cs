6858492f28c2e60e53daec4100643e6220f05e56
david buckley <davidbuckleyweb@outlook.com>
2020-08-09 22:55:33 +0100

    Update the project and solution to .net core 3.1

--- src/AspNetCoreUploadingProgress/Program.cs
+++ src/AspNetCoreUploadingProgress/Program.cs
@@ -4,22 +4,20 @@
 using System.IO;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace AspNetCoreFileUploading
 {
-  public class Program
-  {
-    public static void Main(string[] args)
-    {
-      Program.BuildWebHost(args).Run();
-    }
+   
+        public class Program {
+            public static void Main(string[] args) {
+                CreateHostBuilder(args).Build().Run();
+            }
 
-    public static IWebHost BuildWebHost(string[] args) =>
-      WebHost.CreateDefaultBuilder(args)
-        .UseContentRoot(Directory.GetCurrentDirectory())
-        .UseKestrel()
-        .UseIISIntegration()
-        .UseStartup<Startup>()
-        .Build();
-  }
-}
+            public static IHostBuilder CreateHostBuilder(string[] args) =>
+                Host.CreateDefaultBuilder(args)
+                    .ConfigureWebHostDefaults(webBuilder => {
+                        webBuilder.UseStartup<Startup>();
+                    });
+        }
+    }
